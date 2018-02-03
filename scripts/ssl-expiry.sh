1daf765e7d1de1a216af318cae420c09c1e32841
Schermer,Maarten M.J <me@maartenschermer.com>
2018-02-03 19:27:32 +0100

    combined all scripts in webstatus

--- scripts/ssl-expiry.sh
+++ scripts/ssl-expiry.sh
@@ -6,3 +6,5 @@ fi
 
 EXPIRY_DATE=`echo | openssl s_client -servername $1 -connect $1:443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | sed 's/notAfter=//g' | sed 's/ GMT//g' | sed -E 's/[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}[[:blank:]]+//g'`
 echo $EXPIRY_DATE
+DIR=`dirname $0`
+echo $EXPIRY_DATE | python $DIR/ssl-expiry-to-days.py
--- scripts/whois-expiry.sh
+++ scripts/whois-expiry.sh
@@ -5,4 +5,7 @@ if [ -z "$1" ]; then
 fi
 
 
-whois $1 | grep -i expir | egrep '[0-9]{4}-[0-9]{2}-[0-9]{2}' -o
+EXPIRY_DATE=`whois $1 | grep -i expir | egrep '[0-9]{4}-[0-9]{2}-[0-9]{2}' -o`
+echo $EXPIRY_DATE
+DIR=`dirname $0`
+echo $EXPIRY_DATE | python $DIR/whois-expiry-to-days.py
